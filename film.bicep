0efd70e2666cad3dffa35bb5176c5166052cff2e
AshwinSkariya <ashwin.skariya@s.amity.edu>
2025-03-05 22:56:42 +0530

    latest language

---
+++ film.bicep
@@ -0,0 +1,7 @@
+param num1 int = 10
+param num2 int = 20
+
+var sum = num1 + num2
+
+output result int = sum
+
---
+++ happy.v
@@ -0,0 +1,7 @@
+fn main() {
+    numbers := [10, 20, 30, 40, 50]
+    sum := numbers.sum()
+    average := sum / numbers.len
+    println('Average: $average')
+}
+
